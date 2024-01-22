7d4bbe7ab8532b5767dedc658db62e1d1ede708d
conqueror1988 <fatih.aksoy1988@gmail.com>
2024-01-22 09:58:14 +0100

    finished

---
+++ app.js
@@ -0,0 +1,21 @@
+let addTodoButton = document.getElementById("addTodo");
+
+let todoContainer = document.getElementById("todoContainer");
+
+let inputField = document.getElementById("inputField");
+
+addTodoButton.addEventListener("click", function () {
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = inputField.value;
+  todoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+  paragraph.addEventListener("dblclick", function () {
+    todoContainer.removeChild(paragraph);
+  });
+});
+
+// ! 1. Önce button, div(todoContainer) ve input taglerini id lerinden yakaladik.
